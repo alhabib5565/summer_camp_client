6dd7406d950b2c674161fe2a4efeaa286031ab5c
al-habib <alhabib5565@gmail.com>
2023-06-10 12:24:56 +0600

    install tanstack query

--- src/dashboard/instructors/MyClass.jsx
+++ src/dashboard/instructors/MyClass.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../provider/AuthProvider';
 import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 const MyClass = () => {
     const [classes, setClasses] = useState([])
     const { user } = useContext(AuthContext)
@@ -9,13 +10,43 @@ const MyClass = () => {
             .then(res => res.json())
             .then(data => setClasses(data))
     }, [])
-    // fetch('')
+    
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                 fetch(`http://localhost:5000/removeClas/${id}`, {
+                    method: 'DELETE'
+                 })
+                 .then(res => res.json())
+                 .then(data => {
+                    console.log(data)
+                    if(deletedCount > 0 ){
+
+                          Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                          )
+                    }
+                 })
+            }
+          })
+          
+    }
+
     return (
         <div className='max-w-3xl mx-auto mt-12'>
             <h2 className='text-2xl font-medium'>Hi! <span className='text-purple-500 font-bold'>{user.displayName}</span></h2>
             <div className="overflow-x-auto">
                 <table className="table">
-                    {/* head */}
                     <thead>
                         <tr>
                             <th> #</th>
@@ -44,7 +75,7 @@ const MyClass = () => {
                                 <td>{clas?.status}</td>
                                 <td className='flex items-center gap-4 '>
                                     <button className='btn btn-ghost btn-sm text-white hover:text-gray-700 bg-purple-500'> update</button>
-                                    <button title='delete' className="btn bg-red-500 text-white duration-500 hover:text-gray-700 rounded-full">
+                                    <button onClick={() => handleDelete(clas._id)} title='delete' className="btn bg-red-500 text-white duration-500 hover:text-gray-700 rounded-full">
                                         <FaTrashAlt className='text-lg'></FaTrashAlt>
                                     </button>
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/Routes.jsx'
 import AuthProvider from './provider/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={routes}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routes}></RouterProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
